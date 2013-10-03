39fc3f6c458055dce801e1941170c7f2509efa3f
Mattw46 <matthew_w46@yahoo.com.au>
2013-10-03 11:44:44 +1000

    Update schema.sql
    Small change, moved auction length to auction rather than item

--- schema.sql
+++ schema.sql
@@ -1,13 +1,12 @@
 -- Comp9321 Assignment 2, Group 2
--- Database schema:
-
--- Schema applies to Apache Derby Database
+-- Database schema: (Derby)
 
 CREATE TABLE member (
         username varChar(20) Primary Key,
         nickname varChar(20) NOT NULL,
         firstname varChar(50),
         lastname varChar(50),
+        password varChar(20),
         email varChar(50) NOT NULL,
         birthyear integer,
         address varChar(100),
@@ -17,21 +16,21 @@ CREATE TABLE member (
 
 CREATE TABLE item (
         itemid INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
-        title varChar(30) NOT NULL,
+        title varChar(30) UNIQUE,
         category varChar(15) NOT NULL, -- valid categories to be included in code
         picture varChar(20) NOT NULL, -- filename
         description varChar(100) NOT NULL,
         postagedetails varChar(50) NOT NULL,
         reserveprice integer NOT NULL,
         bidincrement integer NOT NULL,
-        closingtime integer, -- (optional) number hours after auction start
         PRIMARY KEY (itemid)
 );
 
 CREATE TABLE auction (
         item integer NOT NULL,
         username varChar(20) NOT NULL,
-        starttime time NOT NULL,
+        starttime time,
+        auctionlength integer,
         PRIMARY KEY (item, username),
         FOREIGN KEY (item) REFERENCES item(itemid),
         FOREIGN KEY (username) REFERENCES member(username)
@@ -49,14 +48,14 @@ CREATE TABLE bid (
 );
 
 -- start of data
-INSERT INTO member (username, nickname, email, role)
-        VALUES ('Administrator', 'Admin', 'admin@domain.com', 'administrator');
+INSERT INTO member (username, nickname, password, email, role)
+        VALUES ('Administrator', 'Admin', 'admin123', 'admin@domain.com', 'administrator');
 
-INSERT INTO member (username, nickname, firstname, lastname, email, birthyear, address, creditcard, role)
-        VALUES ('user1', 'JohnD', 'John', 'Doe', 'johndoe@missingpersons.com', 1994, '1 Anzac Pde Kensington', 'XXXX-XXXX-XXXX-XXXX', 'member');
+INSERT INTO member (username, nickname, firstname, lastname, password, email, birthyear, address, creditcard, role)
+        VALUES ('user1', 'JohnD', 'John', 'Doe', 'abc123', 'johndoe@missingpersons.com', 1994, '1 Anzac Pde Kensington', 'XXXX-XXXX-XXXX-XXXX', 'member');
 
-INSERT INTO member (username, nickname, firstname, lastname, email, birthyear, address, creditcard, role)
-        VALUES ('user2', 'Jim', 'James', 'Jones', 'jjones@someuni.edu', 1996, '4 George Street Sydney', '1234-5678-0000-1111', 'member');
+INSERT INTO member (username, nickname, firstname, lastname, password, email, birthyear, address, creditcard, role)
+        VALUES ('user2', 'Jim', 'James', 'Jones', 'xyz246', 'jjones@someuni.edu', 1996, '4 George Street Sydney', '1234-5678-0000-1111', 'member');
 
 INSERT INTO item (title, category, picture, description, postagedetails, reserveprice, bidincrement)
         VALUES ('Dell Laptop', 'Computers', 'dell.jpg', 'Scond hand laptop', 'Courier Delivery only', 150, 50);
