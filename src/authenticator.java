76dd5286e7ce37b7acd233c5363b5d1d4003b35b
Mattw46 <matthew_w46@yahoo.com.au>
2013-10-15 11:27:41 +1100

    Update authenticator.java
    Added validation method call. moved accessing username and password to inside this class.

--- src/authenticator.java
+++ src/authenticator.java
@@ -1,4 +1,5 @@
 package edu.unsw.comp9321.assign2;
+
 import java.sql.*;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
@@ -13,28 +14,56 @@ public class authenticator {
 		c = cm.getConnection();
 	}
 	
-	public void login(String user, String password,
-			HttpServletRequest request, HttpServletResponse response) {
+	public void login(HttpServletRequest request, 
+						HttpServletResponse response) {
+		String user = request.getParameter("username");
+		String password = request.getParameter("password");
+		System.out.println("username: " + user + " password: " + password);
 		
-		try {
-			PreparedStatement login = c.prepareStatement("select * " +
-														 "from member " +
-														 "where username=? and password=?");
-			login.setString(1, user);
-			login.setString(2, password);
-			ResultSet result = login.executeQuery();
-			if (result.next()) {
-				RequestDispatcher rd = request.getRequestDispatcher("Main.jsp");
-				rd.forward(request, response);
+		if (validator.checkText(user) && validator.checkPassword(password)) {
+			try {
+				PreparedStatement login = c.prepareStatement("select * " +
+															 "from member " +
+															 "where username=? and password=?");
+				login.setString(1, user);
+				login.setString(2, password);
+				
+				ResultSet result = login.executeQuery();
+				if (result.next()) {
+					System.out.println("Role is: " + result.getString("role"));
+					if (result.getString("role").equals("administrator")) {
+						SessionBean session = new SessionBean(user, "admin");request.setAttribute("SessionTracker", session);
+						RequestDispatcher rd = request.getRequestDispatcher("Admin.jsp");
+						rd.forward(request, response);
+					}
+					else {
+						SessionBean session = new SessionBean(user, "admin"); request.setAttribute("SessionTracker", session);
+						RequestDispatcher rd = request.getRequestDispatcher("BidList.jsp");
+						rd.forward(request, response);
+					}
+				}
+				else {
+					RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+					rd.forward(request, response);
+				}
 			}
-			else {
-				RequestDispatcher rd = request.getRequestDispatcher("LoginFailed.jsp");
-				rd.forward(request, response);
+			catch (Exception e) {
+				System.out.println("An error occured");
+				e.printStackTrace();
 			}
 		}
-		catch (Exception e) {
-			System.out.println("An error occured");
-			e.printStackTrace();
+		else {
+			System.out.println("Validation failed");
+			try {
+				RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+				rd.forward(request, response);
+			}
+			catch (Exception e) {
+				System.out.println("Validation Error occured, redirecting");
+			}
+			
 		}
+		
 	}
+	
 }
