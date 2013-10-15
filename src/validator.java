0eaae670bc96a30a541a0970d37f5d9910cb380a
Mattw46 <matthew_w46@yahoo.com.au>
2013-10-15 11:28:46 +1100

    Create validator.java
    Outline of validate class.

---
+++ src/validator.java
@@ -0,0 +1,27 @@
+package edu.unsw.comp9321.assign2;
+
+/*
+ * validates input from forms
+ */
+public class validator {
+	
+	public static boolean checkText(String text) {
+		
+		return true;
+	}
+	
+	public static boolean checkPassword(String password) {
+		
+		return true;
+	}
+	
+	public static boolean checkNumSequence(String sequence) {
+		
+		return true;
+	}
+	
+	public static boolean checkURL(String url) {
+		
+		return true;
+	}
+}
